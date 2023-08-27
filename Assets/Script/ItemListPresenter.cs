7e455e2af1581c9bc6c2ab557435c75d5f6bc9a8
Neno <nattaphak.udom@kmutt.ac.th>
2023-08-27 13:21:53 +0700

    ADD : ItemListPresenter Script

--- Assets/Script/ItemListPresenter.cs
+++ Assets/Script/ItemListPresenter.cs
@@ -5,66 +5,7 @@ using UnityEngine;
 
 namespace Dota2.ShopSystem
 {
-<<<<<<< HEAD
-    //public class ItemListPresenter : MonoBehaviour
-    //{
-    //    public int currentItemIndex;
-    //    public int currentCategoryIndex;
 
-    //    int maxShownItemCount;
-    //    int maxCategoryCount = 3;
-    //    CurrentPageSize pageSize;
-
-    //    [SerializeField] UIShop ui;
-    //    [SerializeField] Shop shop;
-
-    //    [SerializeField] List<CategoryInfo> categoryInfoList = new List<CategoryInfo>();
-
-    //    void Start()
-    //    {
-    //        RefreshUI();
-    //    }
-
-    //    [ContextMenu(nameof(RefreshUI))]
-    //    void RefreshUI()
-    //    {
-    //        var currentCategoryInfo = categoryInfoList[currentCategoryIndex];
-    //        ui.SetCategory(currentCategoryInfo);
-
-    //        var currentCategory = (ItemType)currentCategoryIndex;
-
-    //        var itemsToDisplay = shop.GetItemsByType(currentCategory);
-    //        maxShownItemCount = itemsToDisplay.Length;
-
-    //        if (maxShownItemCount <= 0)
-    //        {
-    //            ui.ClearAllItemUIs();
-    //            return;
-    //        }
-    //        var currentItem = itemsToDisplay[currentItemIndex];
-    //        ui.SetCurrentItemInfo(currentItem);
-
-    //        var uiDataList = new List<UIItem_Data>();
-
-    //        var currentPageIndex = currentItemIndex / pageSize;
-    //        var startIndexToDisplay = currentPageIndex * pageSize;
-    //        var endIndexToDisplay = startIndexToDisplay + pageSize;
-
-    //        var i = 0;
-    //        foreach (var item in itemsToDisplay)
-    //        {
-    //            if (i >= startIndexToDisplay && i < endIndexToDisplay)
-    //            {
-    //                uiDataList.Add(new UIItem_Data(item, currentItem == item));
-    //            }
-
-    //            i++;
-    //        }
-
-    //        ui.SetItemList(uiDataList.ToArray());
-    //    }
-    //}
-=======
     public class ItemListPresenter : MonoBehaviour
     {
         public int currentItemIndex;
@@ -129,6 +70,4 @@ namespace Dota2.ShopSystem
         //}
 
     }
-    
->>>>>>> origin/ItemListAndPagination
 }
