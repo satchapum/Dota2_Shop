75f823adec7dcf5cf005c69c6f0839c02632fad7
satchapum <satchapum.mail@mail.kmutt.ac.th>
2023-08-27 23:08:06 +0700

    UPDATE: Next Page And Previous Page Script

--- Assets/Script/CurrentPageSize.cs
+++ Assets/Script/CurrentPageSize.cs
@@ -7,19 +7,23 @@ namespace Dota2.ShopSystem
 {
     public class CurrentPageSize : MonoBehaviour
     {
+        [SerializeField] ItemListPresenter refreshUI;
         public int currentPageSize = 10;
 
         public void buttonPageSize10Pressed()
         {
             currentPageSize = 10;
+            refreshUI.RefreshUI();
         }
         public void buttonPageSize20Pressed()
         {
             currentPageSize = 20;
+            refreshUI.RefreshUI();
         }
         public void buttonPageSize30Pressed()
         {
             currentPageSize = 30;
+            refreshUI.RefreshUI();
         }
     }
 }
--- Assets/Script/ItemListPresenter.cs
+++ Assets/Script/ItemListPresenter.cs
@@ -10,29 +10,100 @@ namespace Dota2.ShopSystem
     {
         public int currentItemIndex;
         public int currentCategoryIndex;
-
+        
         int maxShownItemCount;
         int maxCategoryCount = 3;
 
         [Header("PageSize")]
         [SerializeField] CurrentPageSize pageSizeInput;
-        public int currentPagesize;
+        public float currentPagesize;
         [SerializeField] UIShop ui;
         [SerializeField] Shop shop;
 
+        [SerializeField] float currentShopPage = 0.0f;
+        [SerializeField] float maxShopPage = 0.0f;
         [SerializeField] List<CategoryInfo> categoryInfoList = new List<CategoryInfo>();
 
         void Start()
         {
-           RefreshUI();
+            maxShopPage = Mathf.Ceil((shop.Items.Length / currentPagesize)) - 1.0f;
+            RefreshUI();
         }
         void Update()
         {
-            currentPagesize = pageSizeInput.currentPageSize;
+            if (currentPagesize != pageSizeInput.currentPageSize) {
+                currentPagesize = pageSizeInput.currentPageSize;
+                currentShopPage = 0.0f;
+                maxShopPage = Mathf.Ceil((shop.Items.Length / currentPagesize)) - 1.0f;
+                RefreshUI();
+            }
+            
         }
-        [ContextMenu(nameof(RefreshUI))]
-        void RefreshUI()
+        public void PrevItemPageButton()
         {
+            if (currentShopPage == 0)
+            {
+                return;
+            }
+            else
+            {
+                currentShopPage--;
+                ui.ClearAllItemUIs();
+                CreateShopList();
+            }
+            
+        }
+
+        public void NextItemPageButton()
+        {
+
+            if (currentShopPage == maxShopPage)
+            {
+                return;
+            }
+            else
+            {
+                currentShopPage++;
+                ui.ClearAllItemUIs();
+                CreateShopList();
+            }
+        }
+        void CreateShopList()
+        {
+            var currentCategoryInfo = categoryInfoList[currentCategoryIndex];
+
+            var currentCategory = (ItemType)currentCategoryIndex;
+
+            var itemsToDisplay = shop.GetItemsByType(currentCategory);
+            maxShownItemCount = itemsToDisplay.Length;
+
+            if (maxShownItemCount <= 0)
+            {
+                ui.ClearAllItemUIs();
+                return;
+            }
+            var uiDataList = new List<UIItem_Data>();
+
+            var startIndexToDisplay = currentShopPage * currentPagesize;
+            var endIndexToDisplay = startIndexToDisplay + currentPagesize;
+
+            var i = 0;
+            foreach (var item in itemsToDisplay)
+            {
+                if (i >= startIndexToDisplay && i < endIndexToDisplay)
+                {
+                    uiDataList.Add(new UIItem_Data(item));
+                }
+
+                i++;
+            }
+            ui.SetItemList(uiDataList.ToArray());
+        }
+
+        [ContextMenu(nameof(RefreshUI))]
+        public void RefreshUI()
+        {
+
             var currentCategoryInfo = categoryInfoList[currentCategoryIndex];
 
             var currentCategory = (ItemType)currentCategoryIndex;
