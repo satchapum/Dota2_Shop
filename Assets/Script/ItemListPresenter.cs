d69f8ab54fc1b819549c288534540cd9fe74af93
satchapum <satchapum.mail@mail.kmutt.ac.th>
2023-08-27 12:43:34 +0700

    ADD: Script And UI Current Page Button Select

---
+++ Assets/Script/ItemListPresenter.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+namespace Dota2.ShopSystem
+{
+    //public class ItemListPresenter : MonoBehaviour
+    //{
+    //    public int currentItemIndex;
+    //    public int currentCategoryIndex;
+
+    //    int maxShownItemCount;
+    //    int maxCategoryCount = 3;
+    //    CurrentPageSize pageSize;
+
+    //    [SerializeField] UIShop ui;
+    //    [SerializeField] Shop shop;
+
+    //    [SerializeField] List<CategoryInfo> categoryInfoList = new List<CategoryInfo>();
+
+    //    void Start()
+    //    {
+    //        RefreshUI();
+    //    }
+
+    //    [ContextMenu(nameof(RefreshUI))]
+    //    void RefreshUI()
+    //    {
+    //        var currentCategoryInfo = categoryInfoList[currentCategoryIndex];
+    //        ui.SetCategory(currentCategoryInfo);
+
+    //        var currentCategory = (ItemType)currentCategoryIndex;
+
+    //        var itemsToDisplay = shop.GetItemsByType(currentCategory);
+    //        maxShownItemCount = itemsToDisplay.Length;
+
+    //        if (maxShownItemCount <= 0)
+    //        {
+    //            ui.ClearAllItemUIs();
+    //            return;
+    //        }
+    //        var currentItem = itemsToDisplay[currentItemIndex];
+    //        ui.SetCurrentItemInfo(currentItem);
+
+    //        var uiDataList = new List<UIItem_Data>();
+
+    //        var currentPageIndex = currentItemIndex / pageSize;
+    //        var startIndexToDisplay = currentPageIndex * pageSize;
+    //        var endIndexToDisplay = startIndexToDisplay + pageSize;
+
+    //        var i = 0;
+    //        foreach (var item in itemsToDisplay)
+    //        {
+    //            if (i >= startIndexToDisplay && i < endIndexToDisplay)
+    //            {
+    //                uiDataList.Add(new UIItem_Data(item, currentItem == item));
+    //            }
+
+    //            i++;
+    //        }
+
+    //        ui.SetItemList(uiDataList.ToArray());
+    //    }
+    //}
+}
